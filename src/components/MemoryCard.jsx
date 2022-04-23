93a08ea05e15cee4b4c61baf1e36ab7c144b359c
amimbs <andymimbs91@gmail.com>
2022-04-23 00:07:12 -0400

    clickHandler now flips the state of the card. However may not be behaving correctly on the first click

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -2,24 +2,40 @@ import React from "react";
 import './MemoryCard.css';
 
 class MemoryCard extends React.Component {
-    clickeHandler() {
-        alert("card clicked");
-    }
+    constructor(props) {
+        super(props);
+        this.state = { isFlipped: false };
+    };
+
+    // clickeHandler = () => {
+    //     alert("card clicked");
+    //     this.setState(prevState => {
+    //         console.log(prevState)
+    //         return {isFlipped: !prevState.isFlipped}
+    //     },
+    //     () => console.log(this.state))
+    // };
+
+    clickeHandler = (props) => {
+        console.log(this.state.isFlipped)
+        this.setState({isFlipped: !this.state.isFlipped})
+        console.log(this.state.isFlipped, "wtf")
+    };
 
     render() {
         return (
-        <div className="MemoryCard" onClick={this.clickeHandler}>
-            <div className="MemoryCardInner">
-                <div className="MemoryCardBack">
-                    <img src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png" alt="the thing"></img>
-                </div>
-                <div className="MemoryCardFront">
-                    ▲
+            <div className="MemoryCard" onClick={this.clickeHandler}>
+                <div className="MemoryCardInner">
+                    <div className="MemoryCardBack">
+                        <img src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png" alt="the thing"></img>
+                    </div>
+                    <div className="MemoryCardFront">
+                        ▲
+                    </div>
                 </div>
             </div>
-        </div>
         )
-    } 
+    }
 }
 
 export default MemoryCard
