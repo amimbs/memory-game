a1bd0233426a8b553879364e63902e67efca516b
amimbs <andymimbs91@gmail.com>
2022-04-26 17:23:10 -0400

    project completed

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class App extends React.Component {
       return;
     };
 
-    let cardToFlip = {...this.state.deck[cardIndex]};
+    let cardToFlip = { ...this.state.deck[cardIndex] };
 
     cardToFlip.isFlipped = true;
 
@@ -48,12 +48,40 @@ class App extends React.Component {
       }
       return card;
     });
-    
+
+    if (newPickedCards.length === 2) {
+      let card1Index = newPickedCards[0];
+      let card2Index = newPickedCards[1];
+
+      if (newDeck[card1Index].symbol !== newDeck[card2Index].symbol) {
+        setTimeout(this.unflipCards.bind(this, card1Index, card2Index), 1000);
+      };
+      newPickedCards = [];
+    };
+
     this.setState({
       deck: newDeck,
       pickedCards: newPickedCards
     });
-  }
+  };
+
+  unflipCards(card1Index, card2Index) {
+    let card1 = { ...this.state.deck[card1Index] };
+    let card2 = { ...this.state.deck[card2Index] };
+    card1.isFlipped = false;
+    card2.isFlipped = false;
+
+    let newDeck = this.state.deck.map((card, index) => {
+      if (card1Index === index) {
+        return card1;
+      };
+      if (card2Index === index) {
+        return card2
+      }
+      return card;
+    });
+    this.setState({ deck: newDeck });
+  };
 
   render() {
     let cardsJSX = this.state.deck.map((card, index) => {
